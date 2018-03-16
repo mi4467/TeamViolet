f0d4ab19bfd498bf08a3bc4e92f9518f57aafe18
Mustafa Irfan <irfan24@utexas.edu>
2018-03-16 02:43:51 -0700

    ui stuff

--- app/src/main/java/com/hfad/mytimetracker/MainActivity.java
+++ app/src/main/java/com/hfad/mytimetracker/MainActivity.java
@@ -1,7 +1,14 @@
 package com.hfad.mytimetracker;
 
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenuView;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+
+import java.lang.reflect.Field;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("hello");
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        BottomNavigationHelper.removeShiftMode(bottomNavigationView);
+        toolBarSetUp();
     }
+
+    private void toolBarSetUp(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
 }
---
+++ app/src/main/java/com/hfad/mytimetracker/TaskCreatorFragment.java
@@ -0,0 +1,29 @@
+package com.hfad.mytimetracker;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TaskCreatorFragment extends Fragment {
+
+
+    public TaskCreatorFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_task_creator, container, false);
+    }
+
+}
